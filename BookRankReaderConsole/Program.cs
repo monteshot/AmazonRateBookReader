4a70d55a6197b85278dbbece8e40e1b2dea68873
MonteShot <faqreg@gmail.com>
2017-09-10 17:26:52 +0300

    копай яму

--- BookRankReaderConsole/Program.cs
+++ BookRankReaderConsole/Program.cs
@@ -18,61 +18,84 @@ namespace BookRankReaderConsole
             Console.WriteLine("For start reading list of book and collecting data press '1'");
             Console.WriteLine("For start export results to CSV press '2'");
 
-            var key = Console.ReadLine();
-            switch (key)
-            {
-                case "2":
-                    {
-                        Console.WriteLine();
-                        Console.WriteLine("Exporting...");
-                        Console.WriteLine();
-                        //   readFile();
+         //   var key = Console.ReadLine();
+            readFile();
+            createLink();
+            //switch (key)
+            //{
+            //    case "2":
+            //        {
+            //            Console.WriteLine();
+            //            Console.WriteLine("Exporting...");
+            //            Console.WriteLine();
+            //            //   readFile();
 
-                        break;
-                    }
-                case "1":
-                    {
-                        Console.WriteLine();
-                        Console.WriteLine("Reading...");
-                        Console.WriteLine();
-                        readFile();
-                        createLink();
-                        // getBookRate();
-                        printHeader = true;
-                        break;
-                    }
-                default:
-                    Console.WriteLine("Error");
-                    break;
+            //            break;
+            //        }
+            //    case "1":
+            //        {
+            //            Console.WriteLine();
+            //            Console.WriteLine("Reading...");
+            //            Console.WriteLine();
+            //            readFile();
+            //            createLink();
+            //            // getBookRate();
+            //            printHeader = true;
+            //            break;
+            //        }
+            //    default:
+            //        Console.WriteLine("Error");
+            //        break;
 
-            }
+            //}
 
 
         }
 
         static List<string> linkList = new List<string>();
+        static List<string> tempList = new List<string>();
 
 
         static void readFile()
         {
             linkList = new List<string>();
             string[] str = { "\n" };
-            using (StreamReader rd = new StreamReader(new FileStream("list.txt", FileMode.Open)))
+            string[] str1 = { "\n" };
+            try
             {
-                str = rd.ReadToEnd().Split(str, StringSplitOptions.RemoveEmptyEntries);
+                using (StreamReader rd = new StreamReader(new FileStream("list.txt", FileMode.Open)))
+                {
+                    str = rd.ReadToEnd().Split(str, StringSplitOptions.RemoveEmptyEntries);
+                }
+                using (StreamReader rd = new StreamReader(new FileStream("export.csv", FileMode.Open)))
+                {
+                    str1 = rd.ReadToEnd().Split(str1, StringSplitOptions.RemoveEmptyEntries);
+                }
             }
+            catch
+            {
+
+            }
+
             for (int i = 0; i < str.Length; i++)
             {
                 linkList.Add(str[i]);
-
+                // tempList.Add(str[i]);
                 Console.WriteLine(str[i]);
+
+            }
+            for (int i = 0; i < str1.Length; i++)
+            {
+                // linkList.Add(str[i]);
+                tempList.Add(str1[i]);
+                //  Console.WriteLine(str[i]);
 
             }
             Console.WriteLine();
             Console.WriteLine("Complete!\nTotal: " + linkList.Count);
 
-            Main(start);
-
+            //   Main(start);
+            //  return tempList;
         }
 
         static bool printHeader = true;
@@ -93,33 +116,98 @@ namespace BookRankReaderConsole
 
 
         }
+        static void writingInCSVAdder()
+        {
+            //FileStream fs1 = new FileStream("export.csv", FileMode.Append);
+            //StreamWriter writeInCsv = new StreamWriter(fs1, Encoding.Unicode);
+
+            char delimeter = ',';
+            int cnt = 0;
+            foreach (var row in tempList)
+            {
+                int endRowInd = 0;
+                try
+                {
+                    endRowInd = row.IndexOf("\r");
+                    row.Remove(row.IndexOf("\r"));
+                }
+                catch
+                {
+                    endRowInd = row.IndexOf("\n");
+                    row.Remove(row.IndexOf("\n"));
+                }
+
+                row.Insert(endRowInd, ",");
+                if (cnt == 0)
+                {
+                    row.Insert(endRowInd + 1, DateTime.Now.ToString());
+                }
+                else
+                {
+               //     row.Insert(endRowInd + 1, bookRate);
+                }
+                cnt++;
+
+
+                // writeInCsv.WriteLine(row);
+            }
+
+            // writeInCsv.WriteLine(bookNum + delimeter + bookRate + delimeter);
+            //writeInCsv.Close();
+            //fs1.Close();
+
+
+        }
 
         static bool lastBook = false;
         static string[] start;
-      
+
         static string address = "";
 
         static string createLink()
         {
+           FileStream fs1 = new FileStream("export.csv", FileMode.Create);
+            StreamWriter writeInCsv = new StreamWriter(fs1, Encoding.Unicode);
+
             string address = "";
             int cnt = 0;
+
             foreach (var a in linkList)
             {
+               
                 cnt++;
                 string html = "https://www.amazon.com/dp/";
                 html += a;
                 address = html.Substring(0, html.Length - 2);
-                getBookRate(address, a.Substring(0, a.Length - 2));
+                if (tempList != null)
+                {
+                    var b = tempList.Single(m => m.Contains(a.Substring(0, a.Length - 2)));
+                    b += getBookRate(address, a.Substring(0, a.Length - 2));
+                }
+                else
+                {
+                    writingInCSVMethod(getBookRate(address, a.Substring(0, a.Length - 2)), a.Substring(0, a.Length - 2));
+                }
+                
                 if (cnt == linkList.Count)
                 {
                     //lastBook = true;
                     Console.WriteLine("Last book reached");
                     Console.WriteLine("Export complete");
+                    //writingInCSVAdder();
 
-                    Main(start);
+                 //   Main(start);
                 }
 
             }
+
+            //foreach (var row in tempList)
+            //{
+            //    writeInCsv.WriteLine(row);
+            //}
+
+            writeInCsv.Close();
+            fs1.Close();
             return address;
         }
 
@@ -133,7 +221,8 @@ namespace BookRankReaderConsole
             return titles;
         }
 
-        static async void getBookRate(string address, string nameBook)
+        static List<string> ALLrate = new List<string>();
+        static string getBookRate(string address, string nameBook)
         {
             string t = "";
             try
@@ -144,8 +233,13 @@ namespace BookRankReaderConsole
             {
                 Console.WriteLine("Book not found! (" + nameBook + ")");
             }
-            writingInCSVMethod(getNum(t), nameBook);
+          return  getNum(t);
+            //writingInCSVMethod(getNum(t), nameBook);
+           // ALLrate.Add(getNum(t));
+            // return t;
+            //writingInCSVAdder(getNum(t));
         }
+
         static string getNum(string input)
         {
             string output = "";
@@ -162,7 +256,7 @@ namespace BookRankReaderConsole
             {
 
             }
-           return output;
+            return output;
         }
 
     }
